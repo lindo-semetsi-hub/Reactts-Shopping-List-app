210c28221a52d38a633e777057b15b0349c44efa
lindo-semetsi-hub <lindosemetsi@gmail.com>
2025-09-05 12:05:13 +0200

    commit main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import { store } from './store'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const root = createRoot(document.getElementById('root')!)
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+      <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 )

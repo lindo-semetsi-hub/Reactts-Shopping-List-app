d6f29fb38d9126450cc33157bd84b9a75c850431
lindo-semetsi-hub <lindosemetsi@gmail.com>
2026-03-20 15:02:43 +0200

    update

--- src/slices/userSlice.ts
+++ src/slices/userSlice.ts
@@ -3,24 +3,46 @@ import axios from "axios";
 
 const API_URL = "http://localhost:4000/users";
 
+// --------------------------
 export const registerUser = createAsyncThunk(
   "user/register",
-  async (user: any) => {
-    const res = await axios.post(API_URL, user);
+  async (userData: any) => {
+    const res = await axios.post(API_URL, userData);
     return res.data;
+  }
+);
+
+//----------------------------------
+export const loginUser = createAsyncThunk(
+  "user/login",
+  async ({ email, password }: { email: string; password: string }) => {
+    const res = await axios.get(`${API_URL}?email=${email}&password=${password}`);
+    if (res.data.length === 0) throw new Error("Invalid credentials");
+    return res.data[0];
   }
 );
 
 const userSlice = createSlice({
   name: "user",
-  initialState: { currentUser: null, users: [] },
-  reducers: {},
-  extraReducers: builder => {
+  initialState: { currentUser: null as any },
+  reducers: {
+    logout: (state) => {
+      state.currentUser = null;
+    },
+  },
+
+  extraReducers: (builder) => {
     builder.addCase(registerUser.fulfilled, (state, action) => {
-      state.users.push(action.payload);
       state.currentUser = action.payload;
     });
-  }
+    builder.addCase(loginUser.fulfilled, (state, action) => {
+      state.currentUser = action.payload;
+    });
+  },
 });
 
+
+
+
+export const { logout } = userSlice.actions;
 export default userSlice.reducer;
